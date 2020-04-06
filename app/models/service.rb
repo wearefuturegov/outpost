b97e511e0fad57bf127a4a9fe3b7fa8ddf3bb4cc
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-06 12:30:36 +0100

    move short url into helper

--- app/models/service.rb
+++ app/models/service.rb
@@ -6,13 +6,4 @@ class Service < ApplicationRecord
 
   paginates_per 20
   validates_presence_of :name
-
-  def short_url
-    self.url
-      .delete_prefix("https://")
-      .delete_prefix("http://")
-      .delete_prefix("www.")
-      .delete_suffix("/")
-      .truncate(25)
-  end
 end
