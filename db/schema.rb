9dfd7bcf0641a35d20b306f6150008ef2ba481ea
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-06 12:57:24 +0100

    reframe user registration form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_160415) do
+ActiveRecord::Schema.define(version: 2020_04_03_215552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,15 @@ ActiveRecord::Schema.define(version: 2020_04_03_160415) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "watches", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "service_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["service_id"], name: "index_watches_on_service_id"
+    t.index ["user_id"], name: "index_watches_on_user_id"
   end
 
 end
