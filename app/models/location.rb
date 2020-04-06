95ef0b697a9a16029c59022756ce614b1e3d35ec
userman123 <dominic.freeman123@gmail.com>
2020-04-06 12:38:14 +0100

    Remove physical address model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,4 @@
 class Location < ApplicationRecord
-  has_one :physical_address
   has_many :service_at_locations
   has_many :services, through: :service_at_locations
 end
--- app/models/physical_address.rb
+++
@@ -1,3 +0,0 @@
-class PhysicalAddress < ApplicationRecord
-  belongs_to :location
-end
