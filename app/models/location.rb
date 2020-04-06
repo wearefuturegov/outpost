759532fba3e3bd5d02be89c597a869d81f7c5e8e
userman123 <dominic.freeman123@gmail.com>
2020-04-06 13:48:36 +0100

    Add geocoder

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ApplicationRecord
   has_many :service_at_locations
   has_many :services, through: :service_at_locations
+
+  geocoded_by :postal_code
+  after_validation :geocode
 end
