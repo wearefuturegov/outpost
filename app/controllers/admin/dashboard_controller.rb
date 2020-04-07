16ca3bc242c990e2bf458a33af6f70ba5eb4376b
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-07 09:26:52 +0100

    style up dashboard and add some big numbers

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < Admin::BaseController
   
     def index
       @watches = current_user.watches
+      @service_count = Service.count
+      @user_count = User.count
     end
   
   end
