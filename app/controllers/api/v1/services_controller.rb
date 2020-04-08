c3890aa47a58fa2c2122590172dfdce87567a54b
userman123 <dominic.freeman123@gmail.com>
2020-04-08 13:02:45 +0100

    API sort servies by location

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,6 +1,12 @@
 class API::V1::ServicesController < ApplicationController
   def index
-    @services = Service.includes(:organisation).all
+    if params[:coverage].present?
+      @locations = Location.near(params[:coverage]).includes(:services)
+      @services = services_from_locations
+    else
+      @services = Service.includes(:organisation).all
+    end
+
     render json: {
       "totalElements": @services.count,
       "content": serialized_services
@@ -15,4 +21,21 @@ class API::V1::ServicesController < ApplicationController
   def serialized_services
     ActiveModel::Serializer::CollectionSerializer.new(@services)
   end
+
+  def services_from_locations
+    location_ids = @locations.map(&:id)
+
+    @service_at_locations = ServiceAtLocation.where(location: location_ids)
+    @service_at_locations_ordered = @service_at_locations.sort_by{ |service_at_location| location_ids.index service_at_location.location_id }
+
+    @service_at_locations_ordered_service_ids = @service_at_locations_ordered.map(&:service_id)
+
+    service_ids = @service_at_locations.map(&:service_id)
+    @services = Service.where(id: service_ids).includes(:organisation)
+
+    @services_ordered = @services.sort_by{ |service| @service_at_locations_ordered_service_ids.index service.id}
+
+    @services_ordered
+  end
+
 end
