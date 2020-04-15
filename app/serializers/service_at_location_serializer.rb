63686380c8130f07de68936d02460c99826eb983
userman123 <dominic.freeman123@gmail.com>
2020-04-15 11:07:11 +0100

    Add fields to serializer

--- app/serializers/service_at_location_serializer.rb
+++ app/serializers/service_at_location_serializer.rb
@@ -2,6 +2,8 @@ class ServiceAtLocationSerializer < ActiveModel::Serializer
   attribute :service_id, key: :id
   attribute :service_name, key: :name
   attribute :service_description, key: :description
+  attribute :service_email, key: :email
+  attribute :service_url, key: :url
 
   belongs_to :organisation
 end
