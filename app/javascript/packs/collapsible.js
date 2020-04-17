262e350b6045c0aca471befe21a94f6f4f977820
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-17 19:00:04 +0100

    collapsible sections

---
+++ app/javascript/packs/collapsible.js
@@ -0,0 +1,28 @@
+
+let collapsibles = document.querySelectorAll(".collapsible")
+
+collapsibles.forEach(collapsible => {
+    let controls = collapsible.querySelector(".collapsible__header")
+    let content = collapsible.querySelector(".collapsible__content")
+
+    let key = `collapsible_${collapsible.id}`
+
+    const close = () => {
+        controls.setAttribute("aria-expanded", "false")
+        content.setAttribute("hidden", "true")
+        window.localStorage.setItem(key, false)
+    }
+
+    const open = () => {
+        controls.setAttribute("aria-expanded", "true")
+        content.removeAttribute("hidden")
+        window.localStorage.setItem(key, true)
+    }
+
+    window.localStorage.getItem(key) === "true" && open()
+    window.localStorage.getItem(key) === "false" && close()
+
+    controls.addEventListener("click", () => {
+        controls.getAttribute("aria-expanded") === "true" ? close() : open()
+    })
+})
