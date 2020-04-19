2c5f224dcd716658ca67d2a3ebf8ca64cbb7fb60
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-19 11:51:36 +0100

    approvals wip

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,5 @@
 class Admin::BaseController < ApplicationController
     before_action :authenticate_user!
+    # this needs to move out into the application controller
     before_action :set_paper_trail_whodunnit
 end
--- app/controllers/admin/requests_controller.rb
+++ app/controllers/admin/requests_controller.rb
@@ -1,6 +1,5 @@
 class Admin::RequestsController < Admin::BaseController
-
     def index
+        @requests = Approval::Request.all.reverse
     end
-
 end
