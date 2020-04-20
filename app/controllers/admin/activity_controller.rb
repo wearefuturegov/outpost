1dfda4951f377d0f338026c4cf9a36f1bb1000f8
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-20 13:35:55 +0100

    avoid n+1 services problem on activity log and add pagination

--- app/controllers/admin/activity_controller.rb
+++ app/controllers/admin/activity_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ActivityController < Admin::BaseController
     def index
-        @activities = PaperTrail::Version.order("created_at DESC")
+        @activities = PaperTrail::Version
+            .includes(:item)
+            .order("created_at DESC")
+            .page(params[:page])
     end
 end
