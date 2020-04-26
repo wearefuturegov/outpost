db8fb7156665e3eb8c38edb30a0ea0651c28e5fd
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-26 16:23:38 +0100

    add todos

--- app/models/concerns/has_snapshots.rb
+++ app/models/concerns/has_snapshots.rb
@@ -15,6 +15,20 @@ module HasSnapshots
             ]),
             object_changes: self.saved_changes.as_json
         )
+        byebug
         new_snapshot.save
     end
 end
+
+
+# TODOS
+# 1. Attribute changes to current_user or default "System"
+# 2. Handle other events:
+#       - create
+#       - approve
+#       - archive
+#       - unarchive
+# 3. Make sure a snapshot is saved when an old snapshot is restored
+# 4. Make sure all attributes are captured
+# 5. Update UI everywhere
+# 6. Goodbye to paper trail
