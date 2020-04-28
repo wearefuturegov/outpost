17be4bacd2851af9ee84619f611c371d3c1774a3
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-28 22:18:36 +0100

    conditional

--- app/javascript/packs/locations.js
+++ app/javascript/packs/locations.js
@@ -2,31 +2,35 @@ import { initialise } from "./google"
 
 let locationSearch = document.querySelector(".location-search")
 
-let addButton = locationSearch.querySelector("[data-add]")
+if(locationSearch){
 
-addButton.addEventListener("click", e => {
-    e.preventDefault()
-    let time = new Date().getTime()
-    let regexp = new RegExp(addButton.dataset.id, 'g')
-    let results = locationSearch.querySelector(".location-search__results")
-    results.innerHTML += addButton.dataset.fields.replace(regexp, time)
-    
-    results.querySelector("[data-close]").addEventListener("click", e => {
+    let addButton = locationSearch.querySelector("[data-add]")
+
+    addButton.addEventListener("click", e => {
         e.preventDefault()
-        close(e)
+        let time = new Date().getTime()
+        let regexp = new RegExp(addButton.dataset.id, 'g')
+        let results = locationSearch.querySelector(".location-search__results")
+        results.innerHTML += addButton.dataset.fields.replace(regexp, time)
+        
+        results.querySelector("[data-close]").addEventListener("click", e => {
+            e.preventDefault()
+            close(e)
+        })
     })
-})
 
-locationSearch.querySelectorAll("[data-close]").forEach(closer => closer.addEventListener("click", close))
+    locationSearch.querySelectorAll("[data-close]").forEach(closer => closer.addEventListener("click", close))
 
-const close = e => {
-    e.preventDefault()
-    if(window.confirm("Are you sure you want to remove this location?")){
-        let result = e.target.parentNode.parentNode
-        result.setAttribute("hidden", "true")
-        result.querySelector("input[data-destroy-field]").value = "true"
+    const close = e => {
+        e.preventDefault()
+        if(window.confirm("Are you sure you want to remove this location?")){
+            let result = e.target.parentNode.parentNode
+            result.setAttribute("hidden", "true")
+            result.querySelector("input[data-destroy-field]").value = "true"
+        }
     }
 }
+
 
 
 
