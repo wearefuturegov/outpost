a18e2f08de437eebf11d95d46b42767db1a57f71
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-29 13:05:58 +0100

    remove duplicate helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -79,17 +79,6 @@ module ApplicationHelper
         end
     end
 
-    def new_location_fields(f, association)
-      new_object = f.object.send(association).klass.new
-      id = new_object.object_id
-      fields = f.fields_for(association, new_object, child_index: id) do |builder|
-        render("admin/services/editors/location-fields", l: builder)
-      end
-    #   byebug
-      fields.gsub("\n", "")
-    end
-
-
     def link_to_add_fields(name, f, association)
       new_object = f.object.send(association).klass.new
       id = new_object.object_id
@@ -98,6 +87,5 @@ module ApplicationHelper
       end
       link_to(name, '#', class: "button button--small", data: {id: id, fields: fields.gsub("\n", ""), add: true})
     end
-
-
+    
 end
