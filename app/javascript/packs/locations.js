e517664e0ea36e3ec66f90e8027c8f0f03cf9ad3
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-29 13:00:05 +0100

    working add from google feature

--- app/javascript/packs/locations.js
+++ app/javascript/packs/locations.js
@@ -5,37 +5,36 @@ let locationSearch = document.querySelector(".location-search")
 if(locationSearch){
 
     let addButton = locationSearch.querySelector("[data-add]")
+    let results = locationSearch.querySelector(".location-search__results")
 
+    // Add custom
     addButton.addEventListener("click", e => {
         e.preventDefault()
         let time = new Date().getTime()
         let regexp = new RegExp(addButton.dataset.id, 'g')
-        let results = locationSearch.querySelector(".location-search__results")
-        results.innerHTML += addButton.dataset.fields.replace(regexp, time)
 
+        let newResult = document.createElement("li")
+        newResult.classList.add("location-search__result")
+        newResult.innerHTML = addButton.dataset.fields.replace(regexp, time)
+        results.appendChild(newResult)
     })
 
+    // Remove this location
     locationSearch.addEventListener("click", e => {
-        // e.preventDefault()
-        // console.log(e.target)
-
         if(e.target.dataset.close){
             e.preventDefault()
             if(window.confirm("Are you sure you want to remove this location?")){
-                let result = e.target.parentNode.parentNode
+                let result = e.target.parentNode
                 result.setAttribute("hidden", "true")
                 result.querySelector("input[data-destroy-field]").value = "true"
             }
         }
     })
 
-
-
-
-
     let searchInput = locationSearch.querySelector("input[data-google-places-autocomplete]")
     let autocomplete
     
+    // Autocomplete
     const initAutocomplete = async () => {
         await initialise()
         if(searchInput){
@@ -52,6 +51,37 @@ if(locationSearch){
         searchInput.value = ""
         console.log(place)
         // TODO!!!!!
+
+        let time = new Date().getTime()
+        let regexp = new RegExp(addButton.dataset.id, 'g')
+
+        let newResult = document.createElement("li")
+        newResult.classList.add("location-search__result")
+        newResult.innerHTML = addButton.dataset.fields.replace(regexp, time)
+
+        let address = []
+
+        place.address_components.forEach(component => {
+            component.types.includes("premise") && (newResult.querySelector("[data-field='premise']").value = component.long_name)
+
+            component.types.includes("street_number") && address.push(component.long_name)
+            component.types.includes("route") && address.push(component.long_name)
+
+            component.types.includes("postal_town") && (newResult.querySelector("[data-field='postal_town']").value = component.long_name)
+            component.types.includes("locality") && (newResult.querySelector("[data-field='postal_town']").value = component.long_name)
+
+            component.types.includes("postal_code") && (newResult.querySelector("[data-field='postal_code']").value = component.long_name)
+        })
+
+        newResult.querySelector("[data-field='street_number_route']").value = address.join(" ")
+
+        if(place.name) newResult.querySelector("[data-field='premise']").value = place.name
+
+        newResult.querySelector("[data-field='latitude']").value = place.geometry.location.lat()
+        newResult.querySelector("[data-field='longitude']").value = place.geometry.location.lng()
+        newResult.querySelector("[data-field='google_place_id']").value = place.place_id
+
+        results.appendChild(newResult)
     }
 
 }
