7c44d9b5123a38d523e95dd945d4238ff91c7a27
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-29 11:06:35 +0100

    fix closers

--- app/javascript/packs/locations.js
+++ app/javascript/packs/locations.js
@@ -12,47 +12,46 @@ if(locationSearch){
         let regexp = new RegExp(addButton.dataset.id, 'g')
         let results = locationSearch.querySelector(".location-search__results")
         results.innerHTML += addButton.dataset.fields.replace(regexp, time)
-        
-        results.querySelector("[data-close]").addEventListener("click", e => {
-            e.preventDefault()
-            close(e)
-        })
+
     })
 
-    locationSearch.querySelectorAll("[data-close]").forEach(closer => closer.addEventListener("click", e => {
-        e.preventDefault()
-        close(e)
-    }))
+    locationSearch.addEventListener("click", e => {
+        // e.preventDefault()
+        // console.log(e.target)
 
-    const close = e => {
-        if(window.confirm("Are you sure you want to remove this location?")){
-            let result = e.target.parentNode.parentNode
-            result.setAttribute("hidden", "true")
-            result.querySelector("input[data-destroy-field]").value = "true"
+        if(e.target.dataset.close){
+            e.preventDefault()
+            if(window.confirm("Are you sure you want to remove this location?")){
+                let result = e.target.parentNode.parentNode
+                result.setAttribute("hidden", "true")
+                result.querySelector("input[data-destroy-field]").value = "true"
+            }
         }
+    })
+
+
+
+
+
+    let searchInput = locationSearch.querySelector("input[data-google-places-autocomplete]")
+    let autocomplete
+    
+    const initAutocomplete = async () => {
+        await initialise()
+        if(searchInput){
+            autocomplete = new window.google.maps.places.Autocomplete(searchInput)
+            autocomplete.setComponentRestrictions({"country": ["gb"]})
+            autocomplete.addListener("place_changed", handlePlaceChanged)
+        }
+    }
+    
+    initAutocomplete()
+    
+    const handlePlaceChanged = () => {
+        const place = autocomplete.getPlace()
+        searchInput.value = ""
+        console.log(place)
+        // TODO!!!!!
     }
 
 }
-
-
-
-
-//     let searchInput = locationSearch.querySelector("input[data-google-places-autocomplete]")
-//     let autocomplete
-    
-//     const initAutocomplete = async () => {
-//         await initialise()
-//         if(searchInput){
-//             autocomplete = new window.google.maps.places.Autocomplete(searchInput)
-//             autocomplete.setComponentRestrictions({"country": ["gb"]})
-//             autocomplete.addListener("place_changed", handlePlaceChanged)
-//         }
-//     }
-    
-//     initAutocomplete()
-    
-//     const handlePlaceChanged = () => {
-//         const place = autocomplete.getPlace()
-//         searchInput.value = ""
-//         console.log(place)
-//     }
