a3c4c70994bab9a0d0b02f21ce867def7d367102
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-30 21:57:35 +0100

    reactivate tabs js

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,10 +1,7 @@
-// import tabs from "./tabs"
-
+import tabs from "./tabs"
 import collapsible from "./collapsible"
 import filters from "./filters"
 import snapshots from "./snapshots"
-
 import choices from "./choices"
-
 import maps from "./maps"
 import locations from "./locations"
