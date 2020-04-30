ff56343b0582b20ab1eea52963aff200c2a9e8c2
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-04-30 23:19:13 +0100

    tab taxonomy editor

--- app/javascript/packs/tabs.js
+++ app/javascript/packs/tabs.js
@@ -4,13 +4,24 @@ allTabs.forEach(tabs => {
     let controls = tabs.querySelectorAll(".tabs__nav-link")
     let panels = tabs.querySelectorAll(".tabs__panel")
 
-    controls.forEach((control, i) => control.addEventListener("click", e => {
-        e.preventDefault()
+    controls.forEach((control, i) => {
 
-        controls.forEach(c => c.removeAttribute("aria-selected"))
-        panels.forEach(p => p.setAttribute("hidden", "true"))
+        if(i === 0){
+            controls[i].setAttribute("aria-selected", "true")
+        } else {
+            controls[i].setAttribute("aria-selected", "false")
+            panels[i].setAttribute("hidden", "true")
+        }
 
-        control.setAttribute("aria-selected", "true")
-        panels[i].removeAttribute("hidden")
-    }))
+        control.addEventListener("click", e => {
+            e.preventDefault()
+    
+            controls.forEach(c => c.removeAttribute("aria-selected"))
+            panels.forEach(p => p.setAttribute("hidden", "true"))
+    
+            control.setAttribute("aria-selected", "true")
+            panels[i].removeAttribute("hidden")
+        })
+
+    })
 })
