19ee8c23b7482ad15e01fd895f33d6a0327a01a4
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-01 22:53:28 +0100

    improve location display

--- app/javascript/packs/maps.js
+++ app/javascript/packs/maps.js
@@ -1,6 +1,6 @@
 import { initialise } from "./google"
 
-const createMap = async mapHolder => {
+const createSingleMap = async mapHolder => {
     await initialise()
     let {lat, long} = mapHolder.dataset
     let location = new window.google.maps.LatLng(parseFloat(lat), parseFloat(long))
@@ -16,6 +16,36 @@ const createMap = async mapHolder => {
     })
 }
 
+const createListMap = async mapHolder => {
+    await initialise()
+    let bounds = new window.google.maps.LatLngBounds()
+    let map = new window.google.maps.Map(mapHolder, {
+        mapTypeControl: false,
+        streetViewControl: false,
+        zoom: 17
+    })
+    __LOCATIONS__.forEach(location => {
+        let position = new window.google.maps.LatLng(
+            location.latitude,
+            location.longitude
+        )
+        let marker = new window.google.maps.Marker({
+            position: position,
+            map: map,
+        })
+        bounds.extend(position)
+        marker.addListener("click", () => {
+            console.log(location)
+            window.location.href = `/admin/locations/${location.id}`
+        })
+    })
+    map.fitBounds(bounds)
+}
+
 document.querySelectorAll(".map-holder").forEach(m => {
-    createMap(m)
+    if(m.dataset.listMap && __LOCATIONS__){
+        createListMap(m)
+    } else {
+        createSingleMap(m)
+    }
 })
