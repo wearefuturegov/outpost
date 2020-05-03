d058cb389c7e19961442686c6d2993511dcbc29d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-03 13:11:07 +0100

    put prod mailer host in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: ENV["MAILER_HOST"] }
 
   config.action_mailer.delivery_method = :sendgrid_actionmailer
   config.action_mailer.sendgrid_actionmailer_settings = {
