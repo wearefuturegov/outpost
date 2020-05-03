cdd8067f00e7b0c96be68d0c39edab91111368b6
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-03 14:17:54 +0100

    fix tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     email { Faker::Internet.email }
     first_name {Faker::Name.first_name }
     last_name {Faker::Name.last_name }
-    password { "password" }
+    password { "password123A" }
   end
 end
