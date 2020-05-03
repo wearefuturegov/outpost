b773204af5708b702728e9209f34e3b806156b84
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-03 13:32:41 +0100

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,8 +32,7 @@ class User < ApplicationRecord
           :recoverable, 
           :rememberable, 
           :lastseenable, 
-          :secure_validatable, 
-          :session_limitable
+          :secure_validatable
 
 
   def active_for_authentication? 
