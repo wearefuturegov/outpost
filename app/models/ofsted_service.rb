d93d2884cbb56a1f2d0a6bc2b027451e9e75bade
userman123 <dominic.freeman123@gmail.com>
2020-05-12 16:48:52 +0100

    fix pending scopes on ofsted service

--- app/models/ofsted_service.rb
+++ app/models/ofsted_service.rb
@@ -1,6 +1,7 @@
 class OfstedService < Service
-    scope :ofsted_pending, -> { OfstedService.joins(:snapshots).where(snapshots: { action: ['ofsted_create', 'ofsted_update'] } ) }
+    scope :ofsted_unapproved, -> { where(approved: false) }
+    scope :ofsted_pending, -> { ofsted_unapproved.joins(:snapshots).where(snapshots: { action: ['ofsted_create', 'ofsted_update'] } ) }
 
-    scope :ofsted_pending_creates, -> { OfstedService.joins(:snapshots).where(snapshots: { action: 'ofsted_create' } ) }
-    scope :ofsted_pending_updates, -> { OfstedService.joins(:snapshots).where(snapshots: { action: 'ofsted_update' } ) }
+    scope :ofsted_pending_creates, -> { ofsted_unapproved.joins(:snapshots).where(snapshots: { action: 'ofsted_create' } ) }
+    scope :ofsted_pending_updates, -> { ofsted_unapproved.joins(:snapshots).where(snapshots: { action: 'ofsted_update' } ) }
 end
