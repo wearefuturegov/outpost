05aac08d94ea26e55ed7566ee70fe10adcebdaa6
userman123 <dominic.freeman123@gmail.com>
2020-05-13 17:51:14 +0100

    Add distance away attribut to service at location serializer

--- app/serializers/service_at_location_serializer.rb
+++ app/serializers/service_at_location_serializer.rb
@@ -4,6 +4,11 @@ class ServiceAtLocationSerializer < ActiveModel::Serializer
   attribute :service_description, key: :description
   attribute :service_email, key: :email
   attribute :service_url, key: :url
+  attribute :distance, key: :distance_away, if: :location_search?
 
   belongs_to :organisation
+
+  def location_search?
+    object.try(:distance).present?
+  end
 end
