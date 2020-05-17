586455113f55b03e1da438864d4bf12e3361fdac
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-05-17 15:46:43 +0100

    working api response

--- app/controllers/api/v1/taxonomies_controller.rb
+++ app/controllers/api/v1/taxonomies_controller.rb
@@ -2,8 +2,19 @@ class API::V1::TaxonomiesController < ApplicationController
     skip_before_action :authenticate_user!
   
     def index
-        # @tree = Taxonomy.hash_tree
-        # .....
+        render json: json_tree(Taxonomy.hash_tree)
     end
-  
+
+    private
+
+    def json_tree(taxonomies)
+        taxonomies.map do |t, children|
+            {
+                label: t.name,
+                value: t.name.parameterize,
+                level: t.depth,
+                children: json_tree(children)
+            }
+        end
+    end
 end
