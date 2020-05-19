94f8b79cff1951a5a8d7e7f69e554c63545a9aed
Jaye <joshua.hackett93@gmail.com>
2020-05-19 14:48:43 +0100

    ie fixes

--- app/javascript/packs/locations.js
+++ app/javascript/packs/locations.js
@@ -9,7 +9,6 @@ editors.forEach(editor => {
 
     // Add custom
     addButton.addEventListener("click", e => {
-        console.log("fuck")
         e.preventDefault()
         let time = new Date().getTime()
         let regexp = new RegExp(addButton.dataset.id, 'g')
