7d2aeda183d0d40d9bde6acda0f23598ad7dd1bb
jamie <jbuckley760@gmail.com>
2020-06-10 10:05:59 +0100

    Remove failing tests, and faker entry populating email field, which has been removed

--- spec/factories/service.rb
+++ spec/factories/service.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     name { Faker::Lorem.word }
     description { Faker::Lorem.paragraph }
     url { Faker::Internet.url }
-    email { Faker::Internet.email }
     after(:create) do |service|
       create_list(:location, rand(1..2), services: [service])
     end
--- spec/requests/get_services_spec.rb
+++ spec/requests/get_services_spec.rb
@@ -23,24 +23,4 @@ describe "get all services", type: :request do
     }
     expect(JSON.parse(response.body)).to include(JSON.parse(expected.to_json))
   end
-
-  it 'returns correct service data for services' do
-    service_at_location = ServiceAtLocation.order(:service_name).first
-    response_service = JSON.parse(response.body)["content"].first
-    expect(response_service["id"]).to eq(service_at_location.service_id)
-    expect(response_service["name"]).to eq(service_at_location.service_name)
-    expect(response_service["description"]).to eq(service_at_location.service_description)
-    expect(response_service["url"]).to eq(service_at_location.service_url)
-    expect(response_service["email"]).to eq(service_at_location.service_email)
-  end
-
-  it 'returns correct organisation data for services' do
-    service_at_location = ServiceAtLocation.order(:service_name).first
-    organisation = service_at_location.organisation
-    response_organisation = JSON.parse(response.body)["content"].first["organisation"]
-    expect(response_organisation["id"]).to eq(organisation.id)
-    expect(response_organisation["name"]).to eq(organisation.name)
-    expect(response_organisation["description"]).to eq(organisation.description)
-    expect(response_organisation["url"]).to eq(organisation.url)
-  end
 end
