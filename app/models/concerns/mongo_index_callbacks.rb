86cc48ea6c5fb104ea3677b357868596a5e08436
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-06-12 11:22:58 +0100

    insert new index items

--- app/models/concerns/mongo_index_callbacks.rb
+++ app/models/concerns/mongo_index_callbacks.rb
@@ -10,9 +10,9 @@ module MongoIndexCallbacks
         client = Mongo::Client.new(ENV["MONGODB_URI"] || 'mongodb://127.0.0.1:27017/outpost_development')
         collection = client.database[:indexed_services]
         if self.approved?
-            collection.find_one_and_update({id: self.id}, self.as_json)
+            collection.find_one_and_update({id: self.id}, self.as_json, {upsert: true})
         else
-            collection.find_one_and_update({id: self.id}, self.last_approved_snapshot.object)
+            collection.find_one_and_update({id: self.id}, self.last_approved_snapshot.object, {upsert: true})
         end
     end
 
