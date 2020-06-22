619db9ab2ed24641323f2d2a3e1cb4e497b97a57
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-06-22 12:26:13 +0100

    add a validation to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,12 @@
 class Contact < ApplicationRecord
   belongs_to :service
+
+  validate :validate_not_blank
+
+  def validate_not_blank
+    if name.blank? && email.blank? && phone.blank?
+      errors.add(:base, "Each contact needs either a name, email or phone number")
+    end
+  end
+
 end
