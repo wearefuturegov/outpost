56704d712a5c6a67f7701404e81dc199288433a6
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-06-25 11:14:20 +0100

    make ofsted users super-admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -211,6 +211,8 @@ end
 user_logins_yaml = Rails.root.join('lib', 'seeds', 'user_logins.yml')
 user_logins = YAML::load_file(user_logins_yaml)
 user_logins.each do |user_login|
+  user_login.admin_users = true
+  user_login.admin_ofsted = true
   User.create!(user_login) unless (User.where(email: user_login["email"]).size > 0)
 end
 
