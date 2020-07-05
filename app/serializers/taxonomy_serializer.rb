63d8bd4b005733d7cb65939b2d619108cbb743a1
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-07-05 19:45:31 +0100

    add taxonomy parent ids to serialiser

--- app/serializers/taxonomy_serializer.rb
+++ app/serializers/taxonomy_serializer.rb
@@ -2,9 +2,9 @@ class TaxonomySerializer < ActiveModel::Serializer
     attributes :id
     attributes :name
     attributes :slug
+    attributes :parent_id
 
     def slug
         object.name.parameterize
     end
-
 end
