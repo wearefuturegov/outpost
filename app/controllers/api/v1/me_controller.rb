102b395dc7046d08c9de4ec85b41ab6fa8f3244e
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-01 15:07:08 +0100

    require a doorkeeper token to use /me endpoint

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -1,4 +1,6 @@
 class API::V1::MeController < ApplicationController
+    before_action :doorkeeper_authorize!
+    
     def show
         render json: User.find(doorkeeper_token.resource_owner_id)
             .as_json(include: [
