1d4be17cb1a2d6aa503572dece0e30ef853edb8b
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-01 09:35:55 +0100

    add oauth associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,15 @@ class User < ApplicationRecord
           :lastseenable, 
           :secure_validatable
 
+  has_many :access_grants,
+          class_name: 'Doorkeeper::AccessGrant',
+          foreign_key: :resource_owner_id,
+          dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens,
+          class_name: 'Doorkeeper::AccessToken',
+          foreign_key: :resource_owner_id,
+          dependent: :delete_all # or :destroy if you need callbacks
 
   def active_for_authentication? 
     super && kept? 
