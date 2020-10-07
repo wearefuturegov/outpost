7109ce3d9663c3ad303abe6dd2e3bd7e732c00ce
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-07 12:54:40 +0100

    re-style filters

--- app/javascript/packs/filters.js
+++ app/javascript/packs/filters.js
@@ -2,21 +2,7 @@ let allFilters = document.querySelectorAll(".filters")
 
 allFilters.forEach(filters => {
 
-    let control = filters.querySelector(".filters__control")
-    let body = filters.querySelector(".filters__body")
     let form = filters.querySelector(".filters__form")
-
-    control.addEventListener("click", () => {
-        if(control.getAttribute("aria-expanded") === "true"){
-            body.setAttribute("hidden", "true")
-            control.setAttribute("aria-expanded", "false")
-            control.innerHTML = "Filters"
-        } else {
-            body.removeAttribute("hidden")
-            control.setAttribute("aria-expanded", "true")
-            control.innerHTML = "Close filters"
-        }
-    })
 
     form.addEventListener("change", () => {
         form.submit()
