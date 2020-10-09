e62f930ca66bd3d86a4186ddfe9b024bad9c8a3c
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-09 23:19:00 +0100

    better feedback panel styles

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -1,9 +1,5 @@
 class Admin::FeedbacksController < Admin::BaseController
-
     def index
         @feedbacks = Feedback.order(created_at: :desc).page(params[:page])
     end
-
-    private
-
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -6,6 +6,8 @@ class Feedback < ApplicationRecord
 
   after_save :notify_owners
 
+  paginates_per 20
+
   def notify_owners
     ServiceMailer.with(service: self.service).notify_owner_of_feedback_email.deliver_later
   end
