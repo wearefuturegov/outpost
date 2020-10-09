39497138ae960cd0af8cf80e4786a6c37de22fbe
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-10 00:02:34 +0100

    refactor snapshot views

--- app/helpers/snapshots_helper.rb
+++ app/helpers/snapshots_helper.rb
@@ -1,8 +1,7 @@
 module SnapshotsHelper
-
     def ordered_hash(hash)
         if hash.is_a?(Hash)
-            hash.sort_by{|key, value| key}.to_h.map do |key, value|
+            hash.except("updated_at", "created_at", "id").sort_by{|key, value| key}.to_h.map do |key, value|
                 "#{key.humanize}: #{value}"
             end.join("\n")
         else
@@ -11,13 +10,16 @@ module SnapshotsHelper
     end
 
     def ordered(array)
-        array.sort_by{ |o| o["id"]}.map do |element|
-            ordered_hash(element)
-        end.join("\n\n")
+        if array.is_a?(Array)
+            array.sort_by{ |o| o["id"]}.map do |element|
+                ordered_hash(element)
+            end.join("\n\n")
+        else
+            array
+        end
     end
 
     def diff(left, right)
         Diffy::Diff.new(left, right, :allow_empty_diff => false).to_s(:html).html_safe
     end
-    
 end
--- app/helpers/task_list_helper.rb
+++ app/helpers/task_list_helper.rb
@@ -3,13 +3,19 @@ module TaskListHelper
     def task_list_sections
         [
             [
-                "Name and description", "Website and social media"
+                "Name and description", 
+                "Website and social media"
             ],
             [
-                "Categories", "Scheduling", "Fees", "Locations", "Contacts"
+                "Categories", 
+                "Scheduling", 
+                "Fees", 
+                "Locations", 
+                "Contacts"
             ],
             [
-                "Special educational needs and disabilities", "Extra questions"
+                "Special educational needs and disabilities", 
+                "Extra questions"
             ]
         ]
     end
