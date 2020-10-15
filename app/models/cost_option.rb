6e00a2c9c9ac0d0efbf8748a6590aff0db694734
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-10-15 21:19:40 +0100

    fix bug with currency input display

--- app/models/cost_option.rb
+++ app/models/cost_option.rb
@@ -2,4 +2,10 @@ class CostOption < ApplicationRecord
   belongs_to :service
 
   validates_presence_of :amount
+
+  include ActionView::Helpers::NumberHelper
+
+  def amount
+    number_with_precision(self[:amount], precision: 2)
+  end
 end
