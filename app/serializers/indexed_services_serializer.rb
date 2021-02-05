05298f25a0179c0fb60a25615bd64d972723c8c5
Jaye <joshua.hackett93@gmail.com>
2021-02-05 13:53:29 +0000

    merge from master

--- app/serializers/indexed_services_serializer.rb
+++ app/serializers/indexed_services_serializer.rb
@@ -1,6 +1,24 @@
 class IndexedServicesSerializer < ActiveModel::Serializer
   
-  attributes :id, :updated_at, :name, :description, :url, :visible_from, :visible_to, :min_age, :max_age, :age_band_under_2, :age_band_2, :age_band_3_4,:age_band_5_7,:age_band_8_plus,:age_band_all,:needs_referral,:free,:created_at,:status
+  attributes :id, 
+    :updated_at, 
+    :name, 
+    :description, 
+    :url, 
+    :visible_from, 
+    :visible_to, 
+    :min_age, 
+    :max_age, 
+    :age_band_under_2, 
+    :age_band_2, 
+    :age_band_3_4,
+    :age_band_5_7,
+    :age_band_8_plus,
+    :age_band_all,
+    :needs_referral,
+    :free,
+    :created_at,
+    :status
 
   has_many :locations do
     object.locations.where(visible: true)
