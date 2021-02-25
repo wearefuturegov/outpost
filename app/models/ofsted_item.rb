6bf8196a5a5fc78a409b461200e0fe8456bb43b5
userman123 <dominic.freeman123@gmail.com>
2021-02-25 15:15:53 +0000

    Ignore reg status history

--- app/models/ofsted_item.rb
+++ app/models/ofsted_item.rb
@@ -213,7 +213,7 @@ class OfstedItem < ApplicationRecord
 
   # fields we don't care about for version history purposes
   def ignorable_fields
-    ["status", "updated_at", "created_at", "discarded_at", "last_change_date"]
+    ["status", "updated_at", "created_at", "discarded_at", "last_change_date", "registration_status_history"]
   end
   
 end
