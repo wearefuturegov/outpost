ee6bf08fdfa3c0baafa7a04043c4463a345b17d0
userman123 <dominic.freeman123@gmail.com>
2021-03-03 10:51:27 +0000

    Only load mongo connection when needed

--- app/models/concerns/mongo_index_callbacks.rb
+++ app/models/concerns/mongo_index_callbacks.rb
@@ -9,14 +9,11 @@ module MongoIndexCallbacks
     end
 
     def update_index
-        client = Mongo::Client.new(ENV["DB_URI"] || 'mongodb://localhost:27017/outpost_development?authSource=admin', {
-            retry_writes: false
-        })
-        collection = client.database[:indexed_services]
-
         if self.approved? && self.publicly_visible?
+            collection = get_mongo_collection
             collection.find_one_and_update({id: self.id}, IndexedServicesSerializer.new(self).as_json, {upsert: true})
         elsif self.approved && !self.publicly_visible?
+            collection = get_mongo_collection
             collection.find_one_and_delete({id: self.id})
         end
 
@@ -33,5 +30,12 @@ module MongoIndexCallbacks
         #     collection.find_one_and_delete({id: self.id})
         # end
 
+    end
+
+    def get_mongo_collection
+        client = Mongo::Client.new(ENV["DB_URI"] || 'mongodb://localhost:27017/outpost_development?authSource=admin', {
+            retry_writes: false
+        })
+        collection = client.database[:indexed_services]
     end
 end
