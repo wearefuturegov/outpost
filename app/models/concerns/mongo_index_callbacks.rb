b0eabf3e0ddb6f4e32f30774b13b82088de2cb07
Jaye <joshua.hackett93@gmail.com>
2021-03-03 09:56:43 +0000

    mongo callback wip

--- app/models/concerns/mongo_index_callbacks.rb
+++ app/models/concerns/mongo_index_callbacks.rb
@@ -14,18 +14,24 @@ module MongoIndexCallbacks
         })
         collection = client.database[:indexed_services]
 
-        # is the most recent approved version publicly visible?
         if self.approved? && self.publicly_visible?
             collection.find_one_and_update({id: self.id}, IndexedServicesSerializer.new(self).as_json, {upsert: true})
-        
-        # if not, is the last approved snapshot publicly visible?
-        elsif !self.approved? && self.last_approved_snapshot && self.last_approved_snapshot&.object['visible'] && self.last_approved_snapshot&.object['discarded_at'].nil?
-            collection.find_one_and_update({id: self.id}, IndexedServicesSerializer.new(Service.from_hash(self.last_approved_snapshot.object)).as_json, {upsert: true})
-        
-        # if not, delete it from the index
-        else
+        elsif self.discarded_at.present?
             collection.find_one_and_delete({id: self.id})
         end
+
+        # is the most recent approved version publicly visible?
+        # if self.approved? && self.publicly_visible?
+        #     collection.find_one_and_update({id: self.id}, IndexedServicesSerializer.new(self).as_json, {upsert: true})
+        
+        # # if not, is the last approved snapshot publicly visible?
+        # elsif !self.approved? && self.last_approved_snapshot && self.last_approved_snapshot&.object['visible'] && self.last_approved_snapshot&.object['discarded_at'].nil?
+        #     collection.find_one_and_update({id: self.id}, IndexedServicesSerializer.new(Service.from_hash(self.last_approved_snapshot.object)).as_json, {upsert: true})
+         
+        # # if not, delete it from the index
+        # else
+        #     collection.find_one_and_delete({id: self.id})
+        # end
 
     end
 end
