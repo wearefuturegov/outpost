09414c79df00a77b363880901e3dd4b613b0adc3
Jaye <joshua.hackett93@gmail.com>
2021-03-04 12:48:47 +0000

    better function naming

--- app/jobs/update_index_services_job.rb
+++ app/jobs/update_index_services_job.rb
@@ -2,7 +2,7 @@ class UpdateIndexServicesJob < ApplicationJob
     queue_as :default
   
     def perform(service)
-        service.update_index
+        service.update_this_service_in_index
     end
 end
   
