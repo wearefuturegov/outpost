03845b9c4d3bdbfed4d3d5bf72a67b4dbb668d6d
userman123 <dominic.freeman123@gmail.com>
2021-03-16 11:41:40 +0000

    Remove custom field ordering

--- app/models/custom_field_section.rb
+++ app/models/custom_field_section.rb
@@ -1,7 +1,7 @@
 class CustomFieldSection < ApplicationRecord
     validates_presence_of :name, uniqueness: true
 
-    has_many :custom_fields, -> { order(created_at: :asc) }
+    has_many :custom_fields#, -> { order(created_at: :asc) }
     accepts_nested_attributes_for :custom_fields, allow_destroy: true, reject_if: :all_blank
 
     default_scope { order(sort_order: :asc) }
