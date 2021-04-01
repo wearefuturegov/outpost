759b023e0749d96aafb9cdc3393d2344ad3230be
userman123 <dominic.freeman123@gmail.com>
2021-04-01 10:29:01 +0100

    Configure sentry logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
     Sentry.init do |config|
       config.dsn = ENV["SENTRY_DSN"]
       config.breadcrumbs_logger = [:active_support_logger]
-
+      config.logger = Sentry::Logger.new(STDOUT)
       # To activate performance monitoring, set one of these options.
       # We recommend adjusting the value in production:
       config.traces_sample_rate = 0.5
