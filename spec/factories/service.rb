4921993753cde7f4f20f4a6b6e7669084db4b3a6
userman123 <dominic.freeman123@gmail.com>
2021-04-20 14:31:27 +0100

    New service with associations factory to keep separate

--- spec/factories/service.rb
+++ spec/factories/service.rb
@@ -5,7 +5,17 @@ FactoryBot.define do
     url { Faker::Internet.url }
     after(:create) do |service|
       create_list(:location, 1, services: [service])
+    end
+  end
+
+  factory :service_with_all_associations, class: 'Service' do
+    name { Faker::Lorem.word }
+    description { Faker::Lorem.paragraph }
+    url { Faker::Internet.url }
+    after(:create) do |service|
+      create_list(:location, 1, services: [service])
       create_list(:link, 3, service: service)
+      create_list(:send_need, 3, services: [service])
     end
   end
 end
--- spec/tasks/process_permanent_deletions.spec
+++ spec/tasks/process_permanent_deletions.spec
@@ -5,7 +5,7 @@ describe "process permanent deletions" do
 
   before do
     @organisation = FactoryBot.create(:organisation)
-    @services = FactoryBot.create_list(:service, 3, organisation: @organisation)
+    @services = FactoryBot.create_list(:service_with_all_associations, 3, organisation: @organisation)
     @service_for_deletion = @services.last
     @service_for_deletion.update(discarded_at: Time.now - 40.days, marked_for_deletion: Time.now - 31.days)
   end
