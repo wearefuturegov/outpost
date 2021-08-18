1cd75ff6542c3d1ade11cd1b40712d3ffbec0b29
userman123 <dominic.freeman123@gmail.com>
2021-08-18 15:24:41 +0100

    A couple more updates to meet rails conventions

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :taxonomies, only: [:index]
       resources :send_needs, only: [:index]
-      resources :accessibility, only: [:index]
+      resources :accessibilities, only: [:index]
       get "me", to: "me#show"
     end
   end
