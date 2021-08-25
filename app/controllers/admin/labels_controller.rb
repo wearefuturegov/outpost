bd96f4a9959afecc4ff69a655dba55594ab05f6f
userman123 <dominic.freeman123@gmail.com>
2021-08-25 09:39:20 +0100

    Only show label taggings on labels controller

--- app/controllers/admin/labels_controller.rb
+++ app/controllers/admin/labels_controller.rb
@@ -1,6 +1,6 @@
 class Admin::LabelsController < Admin::BaseController
     def index
-        @labels = ActsAsTaggableOn::Tag.most_used(100)
+        @labels = ActsAsTaggableOn::Tag.joins(:taggings).where(taggings: { context: 'labels' }).distinct.most_used(100)
     end
 
     def destroy
