65902d6d1f53408f54aa14774841096e3a141eaf
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2021-08-31 15:19:11 +0100

    Add option to create custom date field

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,14 +1,15 @@
 class CustomField < ApplicationRecord
-    validates_presence_of :key, uniqueness: true
-    validates_presence_of :field_type
-    belongs_to :custom_field_section
+  validates_presence_of :key, uniqueness: true
+  validates_presence_of :field_type
+  belongs_to :custom_field_section
 
-    def self.types
-        [
-            "Text",
-            "Number",
-            "Checkbox",
-            "Select"
-        ]
-    end
+  def self.types
+    [
+      "Text",
+      "Number",
+      "Checkbox",
+      "Select",
+      "Date"
+    ]
+  end
 end
