406ab15b3e96f6a2bac6983adb91e78dacd22319
userman123 <dominic.freeman123@gmail.com>
2021-09-01 17:45:32 +0100

    Add some more tests for admin editing services

--- features/step_definitions/admin_manage_services_steps.rb
+++ features/step_definitions/admin_manage_services_steps.rb
@@ -10,8 +10,8 @@ Given("I am on the add new service page") do
     visit new_admin_service_path
 end
 
-When("I fill in the name") do
-    fill_in('Name', with: 'Test service')
+When("I fill in the name with {string}") do |service_name|
+    fill_in('Name', with: service_name)
 end
 
 When("I select the organisation {string}") do |org_name|
@@ -19,14 +19,26 @@ When("I select the organisation {string}") do |org_name|
     find('.choices__item', text: org_name).click
 end
 
-When("I fill in the suitability field") do
-    find('.checkbox__label', text: 'Autism', visible: false).click
+When("I fill in the suitability field with {string}") do |suit_name|
+    find('.checkbox__label', text: suit_name, visible: false).click
 end
 
 When("I submit the service") do
     click_link_or_button('Create')
 end
 
+When("I update the service") do
+    click_link_or_button('Update')
+end
+
 Then("The service should be created") do
     expect(page).to have_content("Service has been created")
+end
+
+Then("The service should be updated") do
+    expect(page).to have_content("Service has been updated")
+end
+
+Then("The service {string} should have two suitabilities") do |service_name|
+    expect(Service.where(name: service_name).first.suitabilities.count).to eq(2)
 end
