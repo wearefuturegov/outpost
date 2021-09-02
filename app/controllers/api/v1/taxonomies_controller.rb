c84cb370439e2b9b0d85bc197038ca9c356c06f8
Hannah <xhan104@gmail.com>
2021-09-02 10:52:11 +0100

    better error checking for taxonomy filtering

--- app/controllers/api/v1/taxonomies_controller.rb
+++ app/controllers/api/v1/taxonomies_controller.rb
@@ -2,16 +2,23 @@ class API::V1::TaxonomiesController < ApplicationController
     skip_before_action :authenticate_user!
   
     def index
-        # scout sends through lowercase label 'bfis', 'bod' etc - look up the name in app config to send to the application
-        @get_value_from_label = APP_CONFIG['directories'].find{|directory| directory["label"] === params[:directory]}["value"];
-        
-        if params[:directory].present? && APP_CONFIG['directories'].map{|d| d['label']}.include?(params[:directory])
-            render json: json_tree(Taxonomy.filter_by_directory(@get_value_from_label).hash_tree)
-        elsif params[:directory].present? 
-            render json: {'yo': 'ho'}
-        else
+
+        if params[:directory].present?
+
+            # scout sends through lowercase label 'bfis', 'bod' etc - look up the name in app config to send to the application
+            @get_value_from_label = APP_CONFIG['directories'].find{|directory| directory["label"] === params[:directory]};
+
+            if !@get_value_from_label.nil?
+                render json: json_tree(Taxonomy.filter_by_directory(@get_value_from_label["value"]).hash_tree)
+            else 
+                render json: {}
+            end
+
+        else 
             render json: json_tree(Taxonomy.hash_tree)
         end
+        
+       
     end
 
     private
