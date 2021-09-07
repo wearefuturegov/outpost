dc390a08cd519011dc3108b2487e3b1378cc4813
userman123 <dominic.freeman123@gmail.com>
2021-09-07 14:56:02 +0100

    Make sure empty string not saved in directories array

--- app/models/service.rb
+++ app/models/service.rb
@@ -258,7 +258,8 @@ class Service < ApplicationRecord
   end
 
   def update_directories_text_field
-    self.directories_as_text = self.directories.sort.join(", ")
+    self.directories.reject!(&:blank?) # this makes sure there's no empty string added to directories array
+    self.directories_as_text = self.directories.sort.join(", ") # make sure directories always in same order
   end
 
   # include nested taxonomies in json representation by default
