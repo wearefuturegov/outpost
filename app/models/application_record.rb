124f4da385649822c2e3aaae1029ab346b5cdf42
userman123 <dominic.freeman123@gmail.com>
2021-09-13 15:35:36 +0100

    Temp fix for build public indexe

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,6 +6,7 @@ class ApplicationRecord < ActiveRecord::Base
     h.each do |key, value|
       case value.class.to_s
       when 'Array'
+        next if key == "directories" # Ignore old array column 'directories' as not using this anymore and may cause errors
         next unless Object.const_defined?(key.camelize.singularize)
         h[key].map! { |e|
           Object.const_get(key.camelize.singularize).from_hash e }
