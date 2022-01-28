8df46f1a940080c38248bc501a3c1d6234d3c00d
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-01-28 12:49:52 +0100

    Add failing tests for viewing deleted user notes

---
+++ spec/features/viewing_deleted_user_notes_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require 'rake'
+Rails.application.load_tasks
+
+feature 'Viewing deleted user notes' do
+  let(:admin_to_be_deleted) { FactoryBot.create :user, :services_admin, marked_for_deletion: DateTime.now - 40.days }
+  let!(:service) { FactoryBot.create :service }
+  let!(:note) { FactoryBot.create :note, user: admin_to_be_deleted, service: service }
+  let!(:version) { FactoryBot.create :service_version, item_type: 'Service', item_id: service.id, whodunnit: admin_to_be_deleted.id.to_s, event: 'update' }
+
+  before do
+    admin = FactoryBot.create :user, :services_admin
+
+    login_as admin 
+    visit admin_service_path service
+  end
+
+  it 'shows the deleted user name' do
+    within '#service-history' do
+      expect(page).to have_link(admin_to_be_deleted.display_name)
+    end
+
+    within '#service-notes' do
+      expect(page).to have_link(admin_to_be_deleted.display_name)
+      expect(page).to have_content(note.body)
+    end
+
+    Rake.application.invoke_task 'process_permanent_deletions'
+
+    page.refresh
+
+    within '#service-history' do
+      expect(page).to_not have_link(admin_to_be_deleted.display_name)
+      expect(page).to have_content(admin_to_be_deleted.display_name)
+    end
+
+    within '#service-notes' do
+      expect(page).to_not have_link(admin_to_be_deleted.display_name)
+      expect(page).to have_content(admin_to_be_deleted.display_name)
+      expect(page).to have_content(note.body)
+    end
+  end
+
+end
