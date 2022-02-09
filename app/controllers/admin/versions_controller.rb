5170abb6911a3e4e50d4d7c66320dfee5dcd3892
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-02-09 11:26:46 +0100

    Show the deleted user name on the versions page

--- app/controllers/admin/versions_controller.rb
+++ app/controllers/admin/versions_controller.rb
@@ -1,7 +1,7 @@
 class Admin::VersionsController < Admin::BaseController
     def index
         @service = Service.find(params[:service_id])
-        @versions = @service.versions.reverse
+        @versions = @service.versions.includes(:user).reverse
         render :layout => "full-width"
     end
 end
