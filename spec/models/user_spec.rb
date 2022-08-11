535da573edcefa2df68293ca5ba13bce32681b25
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-08-11 15:14:08 +0100

    User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,9 @@ RSpec.describe User, type: :model do
 
     let!(:user) { FactoryBot.build(:user) }
 
-    # it { should validate_presence_of(:email) }
-
-    it "won't save without email" do
-      user.email = nil
-      expect(user.save).to eq(false)
-    end
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
 
     it "won't save without password" do
       user.password = nil
