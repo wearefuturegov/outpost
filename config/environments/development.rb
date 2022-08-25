b3cf2708724297985119ec95d7cec0c503914663
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-08-25 14:22:38 +0100

    rails-7-upgrade

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # Do not eager load code on boot.
+  config.eager_load = false
+
   config.after_initialize do
       Bullet.enable        = true
       Bullet.alert         = true
@@ -10,15 +13,11 @@ Rails.application.configure do
       Bullet.rails_logger  = true
       Bullet.add_footer    = true
     end
-  config.action_mailer.default_url_options = { host: "#{ENV.fetch('PROJECT_NAME')}.localhost", port: 3000 }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Do not eager load code on boot.
-  config.eager_load = false
 
 
   # new
@@ -59,11 +58,11 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "#{ENV.fetch('PROJECT_NAME')}.localhost", port: 3000 }
 
   # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  config.active_support.deprecation = :stderr
 
 
   # Raise exceptions for disallowed deprecations.
