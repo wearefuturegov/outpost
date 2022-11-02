7cef1a24281012b379bbe2e782ac2cd0ce147bba
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2022-11-02 14:30:15 +0000

    Add test for updating site settings

--- spec/features/editing_site_settings_spec.rb
+++ spec/features/editing_site_settings_spec.rb
@@ -19,10 +19,20 @@ feature 'Editing site settings' do
 
     before { login_as admin }
 
-    it 'can view and edit site settings' do
+    it 'can view site settings' do
       visit edit_admin_settings_path
       expect(page).to have_current_path edit_admin_settings_path
       expect(page).to have_content 'Outpost title'
+    end
+
+    it 'can edit site settings' do
+      site_name = 'My service directory'
+      visit edit_admin_settings_path
+      fill_in :form_admin_settings_outpost_title, with: site_name
+      click_button 'Save'
+      expect(page).to have_content 'Settings have been saved'
+      visit root_path
+      expect(page).to have_content site_name
     end
   end
 end
