6ca3b7503e22e1a1a39545304a07f2f47083c60b
Han <apricot13@users.noreply.github.com>
2023-03-10 20:23:02 +0000

    Revert "Docker environment for Outpost" for some reason tower pushed to develop and not my feature branch
    This reverts commit 03871d35fb39f7dd6e0f8e21cf29c9bf6f55c8b8.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
 yaml = Rails.root.join('db', '_seed.yml')
 data = YAML::load_file(yaml)
 
-sample_yaml = Rails.root.join('db', '_sample-data.yml')
-sample_data = YAML::load_file(sample_yaml)
-
-
-import_sample_data = ENV["IMPORT_SAMPLE_DATA"] || false;
-
+# use our realistic sample UK data - fake data won't correctly geocode
+data["locations"].each do |l|
+    Location.create!({
+        address_1: l["address_1"],
+        city: l["city"],
+        postal_code: l["postal_code"],
+        latitude: l["latitude"],
+        longitude: l["longitude"],
+        skip_mongo_callbacks: true
+    })
+end
 
 data["accessibilities"].each do |n|
     Accessibility.create!({name: n})
@@ -20,78 +25,49 @@ data["suitabilities"].each do |n|
     Suitability.create!({name: n})
 end
 
-#make a single admin user
+10.times do 
+    taxon = Taxonomy.create!({
+        name: Faker::Lorem.words(number: rand(1...5)).join(' ').capitalize
+    })
+end
+
+10.times do
+    org = Organisation.create!({
+        name: Faker::Company.name,
+        skip_mongo_callbacks: true
+    })
+
+    rand(0...2).times do 
+        user = User.create!({
+            first_name: Faker::Name.first_name,
+            last_name: Faker::Name.last_name,
+            email: Faker::Internet.email(domain: "example.com"),
+            organisation: org,
+            password: "FakePassword1!"
+        })
+    end
+
+    rand(0...5).times do 
+        service = Service.create!({
+            name: Faker::Company.name,
+            organisation: org,
+            description: Faker::Lorem.paragraph,
+            skip_mongo_callbacks: true
+        })
+        # byebug
+        service.locations << Location.take
+        service.taxonomies << Taxonomy.take
+        service.save!
+    end
+end
+
+# make a single admin user
 User.create!({
     first_name: "Example",
     last_name: "Admin",
     admin: true,
     admin_users: true,
     admin_ofsted: true,
-    email: ENV["INITIAL_ADMIN_EMAIL"] || "example@example.com",
+    email: "example@example.com",
     password: ENV["INITIAL_ADMIN_PASSWORD"] || "FakePassword1!"
 })
-
-
-if import_sample_data
-
-    # use our realistic sample UK data - fake data won't correctly geocode
-    sample_data["locations"].each do |l|
-        Location.create!({
-            address_1: l["address_1"],
-            city: l["city"],
-            postal_code: l["postal_code"],
-            latitude: l["latitude"],
-            longitude: l["longitude"],
-            skip_mongo_callbacks: true
-        })
-    end
-
-    10.times do 
-        taxon = Taxonomy.create!({
-            name: Faker::Lorem.words(number: rand(1...5)).join(' ').capitalize
-        })
-    end
-
-    # subcategories
-    taxonomy_ids = Taxonomy.where.not(parent_id: nil).last(10).map(&:id)
-    taxonomy_ids.each do | t |
-        rand(0...10).times do 
-            child_taxon = Taxonomy.create!({
-                name: Faker::Lorem.words(number: rand(1...5)).join(' ').capitalize,
-                parent_id: t
-            })
-        end
-    end
-
-    10.times do
-        org = Organisation.create!({
-            name: Faker::Company.name,
-            skip_mongo_callbacks: true
-        })
-
-        rand(0...2).times do 
-            user = User.create!({
-                first_name: Faker::Name.first_name,
-                last_name: Faker::Name.last_name,
-                email: Faker::Internet.email(domain: "example.com"),
-                organisation: org,
-                password: "FakePassword1!"
-            })
-        end
-
-        rand(0...5).times do 
-            service = Service.create!({
-                name: Faker::Company.name,
-                organisation: org,
-                description: Faker::Lorem.paragraph,
-                skip_mongo_callbacks: true
-            })
-            # byebug
-            service.locations << Location.take
-            service.taxonomies << Taxonomy.take
-            service.save!
-        end
-    end
-
-
-end
--- environment/docker-run-production.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ash
-
-cd /app
-rm -f /app/tmp/pids/server.pid
-rails s -u puma -p 3000 -b=0.0.0.0
