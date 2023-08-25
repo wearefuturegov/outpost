27f287236d6c2861ac2c0cfde2dff9d362b6cb4d
Han <apricot13@users.noreply.github.com>
2023-08-25 20:16:51 +0100

    tweak to seed data and docker compose setup for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,30 +38,32 @@ if seed_dummy_data
 
     # use our realistic sample UK data - fake data won't correctly geocode
     dummy_data["locations"].each do |l|
-        Location.find_or_create_by!({
+        location = Location.find_or_create_by!({
             address_1: l["address_1"],
             city: l["city"],
             postal_code: l["postal_code"],
             latitude: l["latitude"],
-            longitude: l["longitude"],
-            skip_mongo_callbacks: true
-        })
+            longitude: l["longitude"]
+        }) do |loc|
+            loc.skip_mongo_callbacks = true
+        end
+        location.save
     end
 
     10.times do 
-        taxon = Taxonomy.find_or_create_by!({
+        taxon = Taxonomy.create!({
             name: Faker::Lorem.words(number: rand(1...5)).join(' ').capitalize
         })
     end
 
     10.times do
-        org = Organisation.find_or_create_by!({
+        org = Organisation.create!({
             name: Faker::Company.name,
             skip_mongo_callbacks: true
         })
 
         rand(0...2).times do 
-            user = User.find_or_create_by!({
+            user = User.create!({
                 first_name: Faker::Name.first_name,
                 last_name: Faker::Name.last_name,
                 email: Faker::Internet.email(domain: "example.com"),
@@ -71,7 +73,7 @@ if seed_dummy_data
         end
 
         rand(0...5).times do 
-            service = Service.find_or_create_by!({
+            service = Service.create!({
                 name: Faker::Company.name,
                 organisation: org,
                 description: Faker::Lorem.paragraph,
