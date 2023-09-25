de6c86db6a4f636122412f9dfa02690a3b608d9b
Han <apricot13@users.noreply.github.com>
2023-09-25 20:54:51 +0100

    Adds a wysiwyg editor to the description field for services

---
+++ app/javascript/packs/wysiwyg.js
@@ -0,0 +1,51 @@
+import Trix from "trix";
+
+const simple = document.querySelectorAll(".wysiwyg-simple");
+const { lang } = Trix.config;
+
+/**
+ * Unfortunately this is the only config option available right now with trix
+ * @returns String
+ */
+const outpostWysiwygToolbar = () => {
+  return `<div class="trix-button-row">
+      <span class="trix-button-group trix-button-group--text-tools" data-trix-button-group="text-tools">
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-bold" data-trix-attribute="bold" data-trix-key="b" title="${lang.bold}" tabindex="-1">${lang.bold}hello</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-italic" data-trix-attribute="italic" data-trix-key="i" title="${lang.italic}" tabindex="-1">${lang.italic}</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-strike" data-trix-attribute="strike" title="${lang.strike}" tabindex="-1">${lang.strike}</button>
+      </span>
+
+      <span class="trix-button-group trix-button-group--block-tools" data-trix-button-group="block-tools">
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-quote" data-trix-attribute="quote" title="${lang.quote}" tabindex="-1">${lang.quote}</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-bullet-list" data-trix-attribute="bullet" title="${lang.bullets}" tabindex="-1">${lang.bullets}</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-number-list" data-trix-attribute="number" title="${lang.numbers}" tabindex="-1">${lang.numbers}</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-decrease-nesting-level" data-trix-action="decreaseNestingLevel" title="${lang.outdent}" tabindex="-1">${lang.outdent}</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-increase-nesting-level" data-trix-action="increaseNestingLevel" title="${lang.indent}" tabindex="-1">${lang.indent}</button>
+      </span>
+
+      <span class="trix-button-group-spacer"></span>
+
+      <span class="trix-button-group trix-button-group--history-tools" data-trix-button-group="history-tools">
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-undo" data-trix-action="undo" data-trix-key="z" title="${lang.undo}" tabindex="-1">${lang.undo}</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-redo" data-trix-action="redo" data-trix-key="shift+z" title="${lang.redo}" tabindex="-1">${lang.redo}</button>
+      </span>
+    </div>`;
+};
+
+if (simple.length > 0) {
+  document.addEventListener("trix-before-initialize", () => {
+    // update toolbar
+    Trix.config.toolbar.getDefaultHTML = outpostWysiwygToolbar;
+  });
+  // create the <trix-editor> element
+  // hide the element its actually filling out
+  simple.forEach((s) => {
+    const sibling = s;
+    const editor = document.createElement("trix-editor");
+    editor.classList.add("trix-content");
+    editor.setAttribute("input", sibling.id);
+    editor.id = `${sibling.id}_trix_content`;
+    sibling.style.display = "none";
+    sibling.after(editor);
+  });
+}
