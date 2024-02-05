c2d192704c5cd3fc59de379aa6930ada9be61c75
Madeleine Williams <maddie.williams@tpximpact.com>
2024-02-05 09:44:37 +0000

    Add nested associations to access location data

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -4,13 +4,27 @@ class API::V1::MeController < ApplicationController
     
     def show
         render json: User.find(doorkeeper_token.resource_owner_id)
-            .as_json(include: [
-                organisation: { 
-                    only: [:id, :name], 
-                    include: [
-                        services: {only: [:id, :name]}
-                    ]
+            .as_json(include: {
+                organisation: {
+                  only: [:id, :name],
+                  include: {
+                    services: {
+                      only: [:id, :name],
+                      include: {
+                        service_at_locations: {
+                          only: [:id],
+                          include: {
+                            location: {
+                              only: [:id, :name, :address_1, :city, :postal_code]
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
                 }
-            ])
+              })
+        
+    
     end
 end
