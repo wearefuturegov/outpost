909b1d53daffad43ba3b17604c377f358bf025de
Han <apricot13@users.noreply.github.com>
2024-03-04 18:02:05 +0000

    remove environment plugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,3 @@
 const { environment } = require("@rails/webpacker");
-const webpack = require("webpack");
-
-environment.plugins.prepend(
-  "Environment",
-  new webpack.EnvironmentPlugin(["GOOGLE_CLIENT_KEY"])
-);
 
 module.exports = environment;
