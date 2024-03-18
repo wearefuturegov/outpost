ba85c058f7cd0b8799dbe5c60dc2843423d2109e
Han <apricot13@users.noreply.github.com>
2024-03-18 16:32:08 +0000

    add send_needs

--- app/models/send_need.rb
+++ app/models/send_need.rb
@@ -3,11 +3,23 @@ class SendNeed < ApplicationRecord
 
     validates :name, presence: true, uniqueness: true
 
+    paginates_per 20
+
     def display_name
         name.humanize
     end
 
     def slug
         name.parameterize
+    end
+
+    def self.defaults
+        [
+            "Autism",
+            "Hearing impairment",
+            "Visual impairment",
+            "Mobility",
+            "Cognitive"
+        ]
     end
 end
