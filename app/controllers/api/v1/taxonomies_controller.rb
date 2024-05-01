524c2f5b488dbc1d47e67d00f1b2417380c338d9
Han <apricot13@users.noreply.github.com>
2024-05-01 16:56:38 +0100

    fix tests

--- app/controllers/api/v1/taxonomies_controller.rb
+++ app/controllers/api/v1/taxonomies_controller.rb
@@ -6,7 +6,7 @@ class API::V1::TaxonomiesController < ApplicationController
         directory_labels = params[:directory]
         directories = Directory.where(label: directory_labels)
         if directory_labels.present? && directories.present?
-            render json: json_tree(Taxonomy.filter_by_directory(directory_labels).hash_tree).to_json
+            render json: json_tree(Taxonomy.filter_by_directory(directories.pluck('name')).hash_tree).to_json
         else
             render json: json_tree(Taxonomy.hash_tree).to_json
         end
