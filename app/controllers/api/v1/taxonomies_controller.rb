a8782da703a6619d7401fc4d8ed03ed17161fe8b
Han <apricot13@users.noreply.github.com>
2024-05-01 15:50:10 +0100

    change targetDirectories to be directories

--- app/controllers/api/v1/taxonomies_controller.rb
+++ app/controllers/api/v1/taxonomies_controller.rb
@@ -1,10 +1,12 @@
 class API::V1::TaxonomiesController < ApplicationController
     skip_before_action :authenticate_user!
 
+    # ?directory[]=bfis&directory[]=bod
     def index
-        directory = Directory.find_by(label: params[:directory])
-        if params[:directory].present? && directory
-            render json: json_tree(Taxonomy.filter_by_directory(directory.name).hash_tree).to_json
+        directory_labels = params[:directory]
+        directories = Directory.where(label: directory_labels)
+        if directory_labels.present? && directories.present?
+            render json: json_tree(Taxonomy.filter_by_directory(directory_labels).hash_tree).to_json
         else
             render json: json_tree(Taxonomy.hash_tree).to_json
         end
