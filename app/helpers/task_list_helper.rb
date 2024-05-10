df8c821df02f50f7206df5bf561d3d467fc184b9
Han <apricot13@users.noreply.github.com>
2024-05-10 15:47:08 +0100

    Update tests and moved some logic back to task_list_sections

--- app/helpers/task_list_helper.rb
+++ app/helpers/task_list_helper.rb
@@ -1,7 +1,7 @@
 module TaskListHelper
 
-    def task_list_sections
-        [
+    def task_list_sections(custom_field_sections)
+        sections = [
             [
                 "Name and description",
                 "Website and social media",
@@ -16,10 +16,13 @@ module TaskListHelper
             ],
             [
                 "Special educational needs and disabilities",
-                "Suitable for",
-                # "Extra questions"
+                "Suitable for"
             ]
         ]
+
+        sections.last << "Extra questions" if custom_field_sections.length > 0
+
+        sections
     end
 
 end
