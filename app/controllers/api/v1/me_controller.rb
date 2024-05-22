ef4debd1749850381d80436f94f7d4b01dfadeba
Han <apricot13@users.noreply.github.com>
2024-05-22 13:35:57 +0100

    BSP-56 include postcode with service information

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -8,7 +8,7 @@ class API::V1::MeController < ApplicationController
                 organisation: { 
                     only: [:id, :name], 
                     include: [
-                        services: {only: [:id, :name]}
+                        services: {only: [:id, :name, :postcode]}
                     ]
                 }
             ])
