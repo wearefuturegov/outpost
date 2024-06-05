5fd5c4a07f3a5215f6383d1c4b08381ac45ae842
Han <apricot13@users.noreply.github.com>
2024-06-05 16:55:26 +0100

    Don't attempt to load images from google if not configured

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :public_google
+  config.active_storage.service = ENV['GCP_PROJECT_ID'].present? ? :public_google : :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
